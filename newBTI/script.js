a3bfb2fc7cf5ea16656461eefafa7f82baf04377
garma21 <32247965+garma21@users.noreply.github.com>
2024-01-06 18:57:18 +0200

    Add files via upload

--- newBTI/script.js
+++ newBTI/script.js
@@ -30,3 +30,38 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    var openPopupButton = document.getElementById("openPopupButton");
+    var popupContainer = document.getElementById("popupContainer");
+    var closePopupButton = document.getElementById("closePopupButton");
+    var overlay = document.createElement('div');
+    overlay.className = 'overlay';
+
+    // Функція відкриття popup
+    function openPopup() {
+        popupContainer.classList.add("active");
+        overlay.classList.add("active");
+        closePopupButton.classList.remove("hidden");
+    }
+
+    // Функція закриття popup
+    function closePopup() {
+        popupContainer.classList.remove("active");
+        overlay.classList.remove("active");
+        closePopupButton.classList.add("hidden");
+    }
+
+    // Додати слухач події для кнопки відкриття popup
+    openPopupButton.addEventListener("click", openPopup);
+
+    // Додати слухач події для кнопки закриття popup
+    closePopupButton.addEventListener("click", closePopup);
+
+    // Додати overlay до body
+    document.body.appendChild(overlay);
+
+    // Додати слухач події для закриття popup при кліці на overlay
+    overlay.addEventListener("click", closePopup);
+});
